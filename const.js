f504f15a989a600ea008eeb310fd4f451a2ade07
FantasmasAch <fragofikus@mail.ru>
2021-01-12 19:50:05 +0300

    дальнеишее изучение констант

---
+++ const.js
@@ -0,0 +1,10 @@
+//научимся делать константы
+const birthday='29.12.1994';
+
+//главное отличие константы от переменной, её нельзя изменить
+//пример выбора цветов
+const COLOR_RED = "#F00"
+const COLOR_BLUE = "#00F"
+const COLOR_GREEN = "#0F0"
+let color = COLOR_RED;
+alert(color);
