2dc1ffb49e64d7985712145a09ce6f1971c4a738
FantsmasAch <fragofikus@mail.ru>
2021-01-15 17:59:38 +0300

    решение задачи, растояние за время

--- 2.js
+++ 2.js
@@ -1,3 +1,11 @@
-let myVariable= 26 % 6;
-document.write(myVariable);
-//пригодится для решения задачи
+
+//Задача
+//скорость 40 миль/ч
+//ввести растояние
+//вывести в консоль время в минутах
+//150/40
+//*60
+function main() {
+  var distance = parseInt(readLine(), 10);
+  console.log((distance / 40) * 60);
+}
